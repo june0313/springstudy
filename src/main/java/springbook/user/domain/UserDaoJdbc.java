a117dd17ea8659ab4a32340cd357c02eb4b078ae
YoungJun <june0313@gmail.com>
2015-10-27 22:55:06 +0900

    [7.1.1] XML 설정을 이용한 분리

--- src/main/java/springbook/user/domain/UserDaoJdbc.java
+++ src/main/java/springbook/user/domain/UserDaoJdbc.java
@@ -7,15 +7,18 @@ import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Wayne on 2015. 9. 9..
  */
 public class UserDaoJdbc implements UserDao {
 	private JdbcTemplate jdbcTemplate;
+	private Map<String, String> sqlMap;
 
 	private RowMapper<User> userMapper = new RowMapper<User>() {
-		@Override public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 			User user = new User();
 			user.setId(rs.getString("id"));
 			user.setName(rs.getString("name"));
@@ -32,30 +35,41 @@ public class UserDaoJdbc implements UserDao {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
-	@Override public void add(final User user) {
-		this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend, email) values(?,?,?,?,?,?,?)",
-			user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail());
+	public void setSqlMap(Map<String, String> sqlMap) {
+		this.sqlMap = sqlMap;
 	}
 
-	@Override public void deleteAll() {
-		this.jdbcTemplate.update("delete from users");
+	@Override
+	public void add(final User user) {
+		this.jdbcTemplate.update(this.sqlMap.get("add"),
+				user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(),
+				user.getLogin(), user.getRecommend(), user.getEmail());
 	}
 
-	@Override public User get(String id) {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-			new Object[] { id }, this.userMapper);
+	@Override
+	public void deleteAll() {
+		this.jdbcTemplate.update(this.sqlMap.get("deleteAll"));
 	}
 
-	@Override public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
+	@Override
+	public User get(String id) {
+		return this.jdbcTemplate.queryForObject(sqlMap.get("get"),
+				new Object[]{id}, this.userMapper);
 	}
 
-	@Override public int getCount() {
-		return this.jdbcTemplate.queryForInt("select count(*) from users");
+	@Override
+	public List<User> getAll() {
+		return this.jdbcTemplate.query(sqlMap.get("getAll"), this.userMapper);
 	}
 
-	@Override public void update(User user) {
-		this.jdbcTemplate.update("update users set name=?, password=?, level=?, login=?, recommend=?, email=? where id=?",
-			user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail(), user.getId());
+	@Override
+	public int getCount() {
+		return this.jdbcTemplate.queryForInt(sqlMap.get("getCount"));
+	}
+
+	@Override
+	public void update(User user) {
+		this.jdbcTemplate.update(sqlMap.get("update"),
+				user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail(), user.getId());
 	}
 }
